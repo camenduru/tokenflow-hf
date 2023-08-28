d7d00c963e0f271f64cf9c18e2918f524f8c564d
Linoy Tsaban <LinoyTsaban@users.noreply.huggingface.co>
2023-08-28 21:47:07 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,46 @@
-import gradio as gr
-import os
-from torchvision.io import read_video,write_video
-def video_identity(video):
-    video, _, _ = read_video(video, output_format="TCHW")
-    return str(type(video))
+########
+# demo #
+########
 
-with gr.Blocks() as demo:
-  vid = gr.Video()
-  vid_out = gr.Video()
-  text = gr.Textbox()
-  vid.upload(fn = video_identity, inputs = [vid], outputs=[text])
-# demo = gr.Interface(video_identity, 
-#                     gr.Video(), 
-#                     "playable_video", 
-#                     # examples=[
-#                     #     os.path.join(os.path.abspath(''), 
-#                     #                  "video/video_sample.mp4")], 
-#                     )
 
-if __name__ == "__main__":
-    demo.launch()
+intro = """
+<h1 style="font-weight: 1400; text-align: center; margin-bottom: 7px;">
+   TokenFlow
+</h1>
+"""
+
+
+
+with gr.Blocks(css="style.css") as demo:
+    
+    gr.HTML(intro)
+
+
+    with gr.Row():
+        input_vid = gr.Video(label="Input Video", interactive=True, elem_id="input_video")
+        output_vid = gr.Image(label="Edited Video", interactive=False, elem_id="output_video")
+        input_vid.style(height=365, width=365)
+        output_vid.style(height=365, width=365)
+    
+    with gr.Group(visible=False) as share_btn_container:
+        with gr.Group(elem_id="share-btn-container"):
+            community_icon = gr.HTML(community_icon_html, visible=True)
+            loading_icon = gr.HTML(loading_icon_html, visible=False)
+            share_button = gr.Button("Share to community", elem_id="share-btn", visible=True)
+        
+   
+    with gr.Row():
+        inversion_progress = gr.Textbox(visible=False, label="Inversion progress")
+        
+
+        
+    with gr.Row():
+        run_button = gr.Button("Edit your video!", visible=True)
+        
+
+    # with gr.Accordion("Advanced Options", open=False):
+
+
+
+demo.queue()
+demo.launch()
